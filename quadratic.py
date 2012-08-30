2a81ab8a6084845c4e88e925ab7f1b72fdcc3b01
Coleman McFarland <coleman.mcfarland@gmail.com>
2012-08-29 22:21:32 -0400

    Fixed numberInput function

--- quadratic.py
+++ quadratic.py
@@ -38,10 +38,29 @@ def numberInput():
         print 'Please provide the value of ', value
         var = raw_input('> ') 
         var = float(var)
-        setattr(equation, value, var) 
+        setattr(equation, value, var)
+
+    print equation.a, ' is the value assigned to \'a\' on the fly with setattr()'
+    print equation.b,  ' is the value assigned to \'b\' on the fly with setattr()'
+    print equation.c, ' is the value assigned to \'c\' on the fly with setattr()'
 
     print 'Calculating...'
-    equation.quadraticSolver()
+
+    cat = equation.a
+    dog = equation.b
+    pig = equation.c
+
+    ## neg_test will be either True or False
+    neg_test = equation.negDiscriminant(cat, dog, pig)
+
+    if neg_test == True:        # gotta use double equals to test for equality, single equal is variable assignment
+        print 'Your discriminant is negative. Please try again.'
+        numberInput()
+    else:
+        ## pass these assigned variables to quadraticSolver(). The first-order argument 'self' is passed automatically.
+        equation.quadraticSolver(cat, dog, pig)
+
+
 
 
 def discCheck(a, b, c):
@@ -55,16 +74,13 @@ def discCheck(a, b, c):
 
 
 loopexit = False
-
+print 'Entering while loop...'
 while loopexit == False:  ## must use double equals to test for equality
 
     numberInput()
+    loopexit == True
 
 print 'ending'
-'''
 
-myAnswer = quadraticSolver(a, b, c)
-print myAnswer
-'''
 
 
